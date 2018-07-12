386b3d7981e90b7b5f41cc832b06388fb737bff0
dylankirby <dylankirby@outlook.com>
2018-07-12 15:31:50 -0700

    add test for shortening url

--- test/test.js
+++ test/test.js
@@ -2,13 +2,10 @@ const assert = require('assert');
 const needle = require('needle');
 
 const base_url = 'http://localhost:3000'
+const data = { url: 'www.google.com' }
 
 describe('Routes', () => {
 	it('takes in a url and returns that url', (done) =>{
-		const data = {
-			url: 'www.google.com'
-		}
-
 		needle.post('http://localhost:3000/api/return', data, (err, res) => {
 			if(err){
 				console.log(err);
@@ -18,5 +15,17 @@ describe('Routes', () => {
 				done();
 			}
 		})
+	});
+
+	it('Takes a url, and returns a shortened URL', (done) => {
+		needle.post(`${base_url}/api/shorten`, data, (err, res) => {
+			if(err){
+				console.log(err);
+				assert(1==2);
+			} else {
+				assert(data.url.length > res.body.url.length);
+				done();
+			}
+		});
 	});
 });
