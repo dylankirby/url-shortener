b3e1d92fcbad39b0af2d12f797a1dfb942dcbec6
dylankirby <dylankirby@outlook.com>
2018-07-17 09:28:01 -0700

    empty input shake

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,6 @@
 const local_url = window.location.href; 
 const target = $("#short");
+const oldUrl = $("#old-url");
 const container = $("#new-url");
 
 $(document).ready(() => {
@@ -7,22 +8,18 @@ $(document).ready(() => {
 		// Checks for url, if none display error message
 		let input = $('#url-input').val()
 		if(input){
+
 			$("#main-container").fadeOut(500, () =>{
 				$.post(`${local_url}api/shorten`, { url: input }, (data) => {
 					let { url } = data;
 					url = local_url + url
-					target.text(url);
+					oldUrl.text(input);
+					target.attr("href", url).text(url);
 					container.fadeTo(500,1);
-				})				
-			})
-
+				});				
+			});
 		} else {
-			//no input, shake the 
+			$(".input").transition("shake").addClass("invalid-input");
 		}
-		
-		
-		//If url, captures input and AJAX to server, changes display to spinner
-
-		//Once response recieved, changes display to show new url
 	});
 });
