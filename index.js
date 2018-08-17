8da1fc735f5537bd5f0f7133c582252187404331
dylankirby <dylankirby@outlook.com>
2018-08-17 14:48:59 -0700

    apply rate limiter to shortening route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit')
 
 const indexRoutes = require('./routes/index');
 
@@ -10,8 +11,16 @@ const indexRoutes = require('./routes/index');
 const app = express();
 mongoose.Promise = global.Promise;
 
+const limiter = new rateLimit({
+	windowMs: 15*60*1000,
+	max: 50,
+	delayMs:0
+});
+
+app.enable('trust proxy');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', limiter)
 
 if(process.env.NODE_ENV == 'production'){
 	mongoose.connect(process.env.MONGO_URI);
